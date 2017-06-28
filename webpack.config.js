5368584e42efafdf5b871ce2963128d5964ddb29
Sam Noedel <sam.noedel@gmail.com>
2017-06-27 17:22:36 -0700

    Add minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
-const path = require('path')
+const path           = require('path')
+const UglifyEsPlugin = require('uglify-es-webpack-plugin')
 
 module.exports = {
   entry: './src/sam.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [ ]
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.plugins.push(new UglifyEsPlugin())
 }
